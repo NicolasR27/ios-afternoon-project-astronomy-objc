7e392ed6e98889e7c7f077e7d412307cdbab5ef7
Nicolas Rios <nickrios33@yahoo.com>
2020-06-02 16:41:48 -0700

    added starting xcode project

---
+++ Astronomy/Astronomy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Astronomy
+//
+//  Created by Nicolas Rios on 6/2/20.
+//  Copyright © 2020 Nicolas Rios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
