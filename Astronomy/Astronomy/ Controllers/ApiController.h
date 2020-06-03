cac9445ee98a4ec2b2c846ad587fe004aa92d66c
Nicolas <nickrios33@yahoo.com>
2020-06-03 15:45:33 -0700

    added  api controller h and . m

---
+++ Astronomy/Astronomy/ Controllers/ApiController.h
@@ -0,0 +1,17 @@
+//
+//  ApiController.h
+//  Astronomy
+//
+//  Created by Nicolas Rios on 6/3/20.
+//  Copyright © 2020 Nicolas Rios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ApiController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Astronomy/Astronomy/ Controllers/ApiController.m
@@ -0,0 +1,13 @@
+//
+//  ApiController.m
+//  Astronomy
+//
+//  Created by Nicolas Rios on 6/3/20.
+//  Copyright © 2020 Nicolas Rios. All rights reserved.
+//
+
+#import "ApiController.h"
+
+@implementation ApiController
+
+@end
