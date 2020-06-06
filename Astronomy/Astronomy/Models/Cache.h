b12fee0271fda5d061f203fdfa451fc0bc0499db
Nicolas Rios <nickrios33@yahoo.com>
2020-06-06 01:22:34 -0700

    Added  the implentation code for the cache.h  and cache . m files

---
+++ Astronomy/Astronomy/Models/Cache.h
@@ -0,0 +1,17 @@
+//
+//  Cache.h
+//  Astronomy
+//
+//  Created by Nicolas Rios on 6/6/20.
+//  Copyright © 2020 Nicolas Rios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Cache : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Astronomy/Astronomy/Models/Cache.m
@@ -0,0 +1,13 @@
+//
+//  Cache.m
+//  Astronomy
+//
+//  Created by Nicolas Rios on 6/6/20.
+//  Copyright © 2020 Nicolas Rios. All rights reserved.
+//
+
+#import "Cache.h"
+
+@implementation Cache
+
+@end
