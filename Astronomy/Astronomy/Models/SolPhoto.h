211fbb126a55893a83cbc668740cd6c62742ef58
Nicolas Rios <nickrios33@yahoo.com>
2020-06-06 01:12:19 -0700

    added photoManifest.swift and astronomy Bridging header

---
+++ Astronomy/Astronomy/Models/SolPhoto.h
@@ -0,0 +1,17 @@
+//
+//  SolPhoto.h
+//  Astronomy
+//
+//  Created by Nicolas Rios on 6/6/20.
+//  Copyright © 2020 Nicolas Rios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SolPhoto : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Astronomy/Astronomy/Models/SolPhoto.m
@@ -0,0 +1,13 @@
+//
+//  SolPhoto.m
+//  Astronomy
+//
+//  Created by Nicolas Rios on 6/6/20.
+//  Copyright © 2020 Nicolas Rios. All rights reserved.
+//
+
+#import "SolPhoto.h"
+
+@implementation SolPhoto
+
+@end
